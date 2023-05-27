603756912baa015c7370afc5ec3e45971477994e
Shreya Singh <66567410+Shreyav23Singh@users.noreply.github.com>
2023-05-27 11:45:30 +0530

    Create UsersList.js

---
+++ Added_CollegeName_USing_ref/UsersList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Card from "../UI/Card";
+import classes from './UsersList.module.css';
+
+const UsersList = props =>{
+
+    return (
+        <Card  className={classes.users}>
+        <ul>
+        {props.users.map((users) =>(
+            <li key={users.id}>
+            {users.name}  
+            
+            (  {users.address}  )
+           ( {users.collName} )
+            (   {users.age} years old)
+            
+            </li>
+
+        ))}
+    </ul>
+    </Card>
+    );
+
+}
+export default UsersList;
